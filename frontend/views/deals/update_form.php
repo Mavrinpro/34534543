42c309c39c673505ecf9ce2c48dfcd6e534240bb
User <mavrin79@mail.ru>
2022-10-11 15:18:57 +0400

    Форма update_form обновлена

--- frontend/views/deals/update_form.php
+++ frontend/views/deals/update_form.php
@@ -4,18 +4,14 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use kartik\select2\Select2;
+
 /** @var yii\web\View $this */
 /** @var app\models\Deals $model */
 /** @var yii\widgets\ActiveForm $form */
 ?>
 
 <div class="deals-form">
-    <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
-        'options' => ['class' => 'g-py-15'],
-        'errorCssClass' => 'text-danger',
-        'successCssClass' => 'text-success',
-    ]); ?>
+    <?php $form = ActiveForm::begin(['id' => 'login-form', 'options' => ['class' => 'g-py-15'], 'errorCssClass' => 'text-danger', 'successCssClass' => 'text-success',]); ?>
     <?php $date = date('Y-m-d H:i:s'); ?>
     <div class="row">
         <div class="col-md-6">
@@ -28,10 +24,8 @@ use kartik\select2\Select2;
     </div>
     <div class="row">
         <div class="col-md-6">
-            <?= $form->field($model, 'tag')->dropDownList(ArrayHelper::map(\app\models\Tags::find()->all(), 'id', 'name'),
-                ['prompt'=>'Теги...']
-            ) ?>
-            <!--            --><?//= $form->field($model, 'tag')->widget(\kartik\select2\Select2::classname(), [
+            <?= $form->field($model, 'tag')->dropDownList(ArrayHelper::map(\app\models\Tags::find()->all(), 'id', 'name'), ['prompt' => 'Теги...']) ?>
+            <!--            --><? //= $form->field($model, 'tag')->widget(\kartik\select2\Select2::classname(), [
             //                'data' => ArrayHelper::map(\app\models\Tags::find()->all(), 'id', 'name'),
             //                'options' => ['placeholder' => 'Select a state ...', 'multiple' => true],
             //                'pluginOptions' => [
@@ -54,22 +48,18 @@ use kartik\select2\Select2;
         </div>
 
         <div class="col-md-6">
-            <?= $form->field($model, 'status')->dropDownList(ArrayHelper::map(\app\models\Statuses::find()->all(), 'id', 'name'),
-                ['prompt'=>'Статус...']); ?>
+            <?= $form->field($model, 'status')->dropDownList(ArrayHelper::map(\app\models\Statuses::find()->all(), 'id', 'name'), ['prompt' => 'Статус...']); ?>
             <?= $form->field($model, 'date_create')->hiddenInput(['value' => $date])->label(false); ?>
-        </div></div>
+        </div>
+    </div>
     <div class="row">
 
         <div class="col-md-6">
 
-            <?= $form->field($model, 'id_operator')->dropDownList(ArrayHelper::map
-            (\common\models\User::find()->all(), 'id', 'username'),
-                ['prompt'=>'Ответственный...']); ?>
+            <?= $form->field($model, 'id_operator')->dropDownList(ArrayHelper::map(\common\models\User::find()->all(), 'id', 'username'), ['prompt' => 'Ответственный...']); ?>
         </div>
         <div class="col-md-6">
-            <?= $form->field($model, 'id_filial')->dropDownList(ArrayHelper::map(\app\models\Branch::find()->all(), 'id', 'name'),
-                ['prompt'=>'Выберите город...']
-            ) ?>
+            <?= $form->field($model, 'id_filial')->dropDownList(ArrayHelper::map(\app\models\Branch::find()->all(), 'id', 'name'), ['prompt' => 'Выберите город...']) ?>
         </div>
     </div>
 
@@ -79,23 +69,18 @@ use kartik\select2\Select2;
             <?= $form->field($model, 'id_comment')->widget(\yii\redactor\widgets\Redactor::className()) ?>
         </div>
         <div class="col-md-6">
-            <?= $form->field($model, 'deal_sum')->textInput(['type'=>'number']) ?>
+            <?= $form->field($model, 'deal_sum')->textInput(['type' => 'number']) ?>
         </div>
 
         <?= $form->field($model, 'del')->hiddenInput(['value' => true])->label(false); ?>
 
     </div>
-    <div class="form-group">
-        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
-        <?= Html::a('<i class="fa fa-trash"></i>', ['deals/updater', 'id' => $model->id], [
-            'class' => ' m-auto btn btn-danger',
-            'data' => [
-                'confirm' => 'Хотите удалить эту сделку?',
-                'method' => 'post',
-            ],
-        ]) ?>
+    <div class="row">
+        <div class="col-md-6 d-flex">
+            <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
+            <?= Html::a('<i class="fa fa-trash"></i>', ['deals/updater', 'id' => $model->id], ['class' => ' ml-auto btn btn-danger', 'data' => ['confirm' => 'Хотите удалить эту сделку?', 'method' => 'post',],]) ?>
+        </div>
     </div>
-
     <?php ActiveForm::end(); ?>
 
 </div>
