2f6e0567338736c9eef3a574f2169806c548c698
User <mavrin79@mail.ru>
2022-10-19 10:52:09 +0400

    Add AdminLteAsset to frontend

--- backend/views/src/views/layouts/main.php
+++ backend/views/src/views/layouts/main.php
@@ -12,7 +12,7 @@ $this->registerCssFile('https://fonts.googleapis.com/css?family=Source+Sans+Pro:
 
 $assetDir = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
 
-$publishedRes = Yii::$app->assetManager->publish('@vendor/hail812/yii2-adminlte3/src/web/js');
+$publishedRes = Yii::$app->assetManager->publish('@webroot/js');
 $this->registerJsFile($publishedRes[1].'/control_sidebar.js', ['depends' => '\hail812\adminlte3\assets\AdminLteAsset']);
 ?>
 <?php $this->beginPage() ?>
