afa6a8e12e7e7e945eed51f2ddce467fd604f9f1
User <mavrin79@mail.ru>
2022-10-19 10:36:59 +0400

    Add AdminLteAsset to frontend

---
+++ frontend/assets/AdminLteAsset.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AdminLteAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/adminlte.min.css',
+        '/css/site.css',
+    ];
+    public $js = [
+        'js/adminlte.min.js',
+        '/js/Chart.min.js',
+        '/js/custom.js',
+        '//www.amcharts.com/lib/4/core.js',
+        'https://cdn.amcharts.com/lib/4/charts.js',
+        '//www.amcharts.com/lib/4/themes/animated.js',
+        '//www.amcharts.com/lib/4/lang/ru_RU.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap4\BootstrapAsset',
+    ];
+}
