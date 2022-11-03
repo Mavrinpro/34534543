e2485224cb23c9d5c0838091b9b14d5fc855e9b3
User <mavrin79@mail.ru>
2022-11-03 11:25:14 +0400

    Change deals form use yii\bootstrap4\ActiveForm;

--- frontend/views/deals/_form.php
+++ frontend/views/deals/_form.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use yii\bootstrap4\ActiveForm;
 use yii\helpers\ArrayHelper;
 use kartik\select2\Select2;
 use yii\widgets\MaskedInput;
@@ -11,12 +11,7 @@ use yii\widgets\MaskedInput;
 ?>
 
 <div class="deals-form">
-    <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
-        'options' => ['class' => 'g-py-15'],
-        'errorCssClass' => 'text-danger',
-        'successCssClass' => 'text-success',
-    ]); ?>
+    <?php $form = ActiveForm::begin(); ?>
     <?php $date = date('Y-m-d H:i:s'); ?>
     <div class="row">
     <div class="col-md-6">
@@ -50,26 +45,6 @@ use yii\widgets\MaskedInput;
             ]);
             ?>
 
-<!--            --><?//= $form->field($model, 'tag')->widget(\kartik\select2\Select2::classname(), [
-//                'data' => ArrayHelper::map(\app\models\Tags::find()->all(), 'id', 'name'),
-//                'options' => ['placeholder' => 'Select a state ...', 'multiple' => true],
-//                'pluginOptions' => [
-//                    'allowClear' => true
-//                ],
-//            ]); ?>
-            <?php
-//echo '<label class="control-label">Теги</label>';
-//            echo \kartik\select2\Select2::widget([
-//                'name' => 'tag',
-//                 'hideSearch' => true,
-//                'data' => ArrayHelper::map(\app\models\Tags::find()->all(), 'id', 'name'),
-//                'size' => \kartik\select2\Select2::THEME_KRAJEE,
-//                'options' => ['placeholder' => 'Select a state ...', 'multiple' => true, 'autocomplete' => 'off'],
-//                'pluginOptions' => [
-//                    'allowClear' => true
-//                ],
-//            ])
-            ?>
         </div>
 
         <div class="col-md-6">
