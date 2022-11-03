402379b6e3e0f1ef443b158074cac534d5818d49
User <mavrin79@mail.ru>
2022-11-03 15:55:28 +0400

    change tasks index.php

--- frontend/views/tasks/index.php
+++ frontend/views/tasks/index.php
@@ -7,40 +7,40 @@ use yii\grid\ActionColumn;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
 use common\widgets\Alert;
-use Smalot\PdfParser\Parser;
+//use Smalot\PdfParser\Parser;
 /** @var yii\web\View $this */
 /** @var app\models\TasksSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
 // Parse PDF file and build necessary objects.
-$parser = new \Smalot\PdfParser\Parser;
-$pdf = $parser->parseFile("http://cdn.glazcentre.ru/docs/text.pdf");
+//$parser = new \Smalot\PdfParser\Parser;
+//$pdf = $parser->parseFile("http://cdn.glazcentre.ru/docs/text.pdf");
 
-$data = $pdf->getPages()[0]->getDataTm();
+//$data = $pdf->getPages()[0]->getDataTm();
 
 //var_dump($data);
-foreach ($data as $key => $datum) {
-    //echo $datum[1].'<br>';
-}
+//foreach ($data as $key => $datum) {
+//    //echo $datum[1].'<br>';
+//}
 ?>
-Ф.И.О: <?= $data[8][1]. "</br>" ?>
-Пол: <?= $data[10][1]. "</br>" ?>
-Год рождения: <?= $data[12][1]. "</br>" ?>
-Дата взятия биоматериала: <?= $data[14][1]. "</br>" ?>
-<table style="width: 100%;">
-    <thead>
-    <th>
-        <td><?= $data ?></td>
-        <td>shsrth</td>
-        <td>shsrth</td>
-    </th>
-    </thead>
-</table>
+<!--Ф.И.О: --><?//= $data[8][1]. "</br>" ?>
+<!--Пол: --><?//= $data[10][1]. "</br>" ?>
+<!--Год рождения: --><?//= $data[12][1]. "</br>" ?>
+<!--Дата взятия биоматериала: --><?//= $data[14][1]. "</br>" ?>
+<!--<table style="width: 100%;">-->
+<!--    <thead>-->
+<!--    <th>-->
+<!--        <td>--><?//= $data ?><!--</td>-->
+<!--        <td>shsrth</td>-->
+<!--        <td>shsrth</td>-->
+<!--    </th>-->
+<!--    </thead>-->
+<!--</table>-->
 <?php
-
-foreach ($data as $items) {
-
-}
+//
+//foreach ($data as $items) {
+//
+//}
 
 
 
