79e5ead5f6f4ed85768da673e9093509f2608622
User <mavrin79@mail.ru>
2022-11-08 15:39:33 +0400

    change menu

--- common/config/main.php
+++ common/config/main.php
@@ -6,6 +6,15 @@ return [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'container' => [
+        'definitions' => [
+            \yii\widgets\LinkPager::class => \yii\bootstrap4\LinkPager::class,
+            'yii\bootstrap4\LinkPager' => [
+
+
+            ]
+        ],
+    ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'language' => 'ru',
