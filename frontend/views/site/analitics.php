dd6e7b311eb747460b1dbe9a79c071d620e5d51f
keepe <mavrin79@mail.ru>
2022-11-17 21:00:35 +0400

    Install Chart Js site/index chart add

--- frontend/views/site/analitics.php
+++ frontend/views/site/analitics.php
@@ -1,5 +1,6 @@
 <?php
 //use app\models\Branch;
+use dosamigos\chartjs\ChartJs;
 /** @var yii\web\View $this */
 
 $this->title = 'Статистика';
@@ -39,8 +40,8 @@ $this->title = 'Статистика';
     $DATA_GRAF = json_encode($DATA_GRAF);
 
     ?>
-
-<div class="col-md-12">
+<div class="row">
+<div class="col-md-6">
     <div class="card card-info">
         <div id="chartdiv" style="height: 400px;"></div>
         <div id="legenddiv" class="hidden d-none"></div>
@@ -60,7 +61,46 @@ $this->title = 'Статистика';
         </div>
     </div>
 </div>
-
+<div class="col-md-6">
+    <div class="card card-info">
+        <div class="card-body">
+            <?= ChartJs::widget([
+                'type' => 'bar',
+                'options' => [
+                    'height' => 400,
+                    'width' => 400
+                ],
+                'data' => [
+                    'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+                    'datasets' => [
+                        [
+                            'label' => "My First dataset",
+                            'backgroundColor' => "rgba(179,181,198,0.2)",
+                            'borderColor' => "rgba(179,181,198,1)",
+                            'pointBackgroundColor' => "rgba(179,181,198,1)",
+                            'pointBorderColor' => "#fff",
+                            'pointHoverBackgroundColor' => "#fff",
+                            'pointHoverBorderColor' => "rgba(179,181,198,1)",
+                            'data' => [65, 59, 90, 81, 56, 55, 40]
+                        ],
+                        [
+                            'label' => "My Second dataset",
+                            'backgroundColor' => "rgba(255,99,132,0.2)",
+                            'borderColor' => "rgba(255,99,132,1)",
+                            'pointBackgroundColor' => "rgba(255,99,132,1)",
+                            'pointBorderColor' => "#fff",
+                            'pointHoverBackgroundColor' => "#fff",
+                            'pointHoverBorderColor' => "rgba(255,99,132,1)",
+                            'data' => [28, 48, 40, 19, 96, 27, 100]
+                        ]
+                    ]
+                ]
+            ]);
+            ?>
+        </div>
+    </div>
+</div>
+</div>
 <!--<div class="row">-->
 <!--<div class="col-md-6" style="height: 400px;">-->
 <!---->
