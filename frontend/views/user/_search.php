e6e89492e57d3292a1de66432145ca3f99f910e5
User <mavrin79@mail.ru>
2022-12-08 16:58:51 +0400

    Пользователи update

--- frontend/views/user/_search.php
+++ frontend/views/user/_search.php
@@ -30,7 +30,7 @@ use yii\widgets\ActiveForm;
 
     <?php // echo $form->field($model, 'email') ?>
 
-    <?php // echo $form->field($model, 'status') ?>
+    <?php  echo $form->field($model, 'status') ?>
 
     <?php // echo $form->field($model, 'created_at') ?>
 
