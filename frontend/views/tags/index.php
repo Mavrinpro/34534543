b85cd04413f437b354e3b87130f8237722f01d0e
User <mavrin79@mail.ru>
2022-12-08 14:55:40 +0400

    разграничение прав на удаление тегов, филиалов

--- frontend/views/tags/index.php
+++ frontend/views/tags/index.php
@@ -33,6 +33,12 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'class' => ActionColumn::className(),
                 'header' =>    Html::a('Сбросить фильтр', ['tags/index'], ['class' => 'btn btn-sm btn-outline-primary']),
+                'visibleButtons' => [
+
+                    'delete' => function ($model) {
+                        return \Yii::$app->authManager->getRolesByUser(\Yii::$app->getUser()->identity->getId())['superadmin']->name == 'superadmin' || \Yii::$app->authManager->getRolesByUser(\Yii::$app->getUser()->identity->getId())['admin']->name == 'admin';
+                    },
+                ],
                 'buttons' => [
                     'update' => function ($url,$model, $key) {
                         return Html::a(
