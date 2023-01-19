1be08eae54b1ed38f600f886efd71d86c8691e2f
User <mavrin79@mail.ru>
2023-01-19 12:44:38 +0400

    update

--- frontend/views/user/view.php
+++ frontend/views/user/view.php
@@ -75,18 +75,33 @@ $this->params['breadcrumbs'][] = $this->title;
 //        $password = 'rEL2bpVR2hVdSc9b8R2L0hwjb';
 //    $hash = Yii::$app->getSecurity()->generatePasswordHash($password);
 //    echo $hash;
-//$called = '8422271131';
-//    function formatPhone($num) {
-//        $num = preg_replace('/[^0-9]/', '', $num);
-//        $len = strlen($num);
-//
-//        if($len == 11) $num = preg_replace('/([0-9]{1})([0-9]{3})([0-9]{3})/', '7$2$3', $num);
-//        //elseif($len == 8) $num = preg_replace('/([0-9]{3})([0-9]{2})([0-9]{3})/', '$1 - $2 $3', $num);
-//        //elseif($len == 9) $num = preg_replace('/([0-9]{3})([0-9]{2})([0-9]{2})([0-9]{2})/', '$1 - $2 $3 $4', $num);
-//        elseif($len == 10) $num = preg_replace('/([0-9]{3})([0-9]{2})([0-9]{2})([0-9]{3})/', '7$1$2$3$4', $num);
-//
-//        return $num;
-//    }
-//    echo formatPhone($called);
+$called = '8423521461';
+    function formatPhone($num) {
+        $num = preg_replace('/[^0-9]/', '', $num);
+        $len = strlen($num);
+
+        if($len == 11) $num = preg_replace('/([0-9]{1})([0-9]{3})([0-9]{3})/', '7$2$3', $num);
+        //elseif($len == 8) $num = preg_replace('/([0-9]{3})([0-9]{2})([0-9]{3})/', '$1 - $2 $3', $num);
+        //elseif($len == 9) $num = preg_replace('/([0-9]{3})([0-9]{2})([0-9]{2})([0-9]{2})/', '$1 - $2 $3 $4', $num);
+        elseif($len == 10) $num = preg_replace('/([0-9]{3})([0-9]{2})([0-9]{2})([0-9]{3})/', '7$1$2$3$4', $num);
+
+        return $num;
+    }
+    echo formatPhone($called);
+
+    $br = 'Входящая COPY1';
+
+    switch ($br){
+        case 'Входящая':
+            $tagName = '25';
+            break;
+        case 'Входящая COPY1':
+            $tagName = '26';
+            break;
+        case 'ЧС':
+            $tagName = '27';
+            break;
+    }
+    echo $tagName;
     ?>
 </div>
