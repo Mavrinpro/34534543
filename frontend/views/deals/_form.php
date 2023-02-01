1d9310fce804fd4a5417b2196dbda276222775c8
User <mavrin79@mail.ru>
2023-02-01 16:36:10 +0400

    update

--- frontend/views/deals/_form.php
+++ frontend/views/deals/_form.php
@@ -110,8 +110,6 @@ use yii\widgets\MaskedInput;
 
             <?= $form->field($model, 'del')->hiddenInput(['value' => 0])->label(false); ?>
             <?= $form->field($model, 'create_form')->hiddenInput(['value' => 0])->label(false); ?>
-
-
     </div>
     <div class="form-group">
         <?= Html::submitButton('Добавить', ['class' => 'btn btn-success']) ?>
